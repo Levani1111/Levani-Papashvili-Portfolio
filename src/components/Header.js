06fadc908c432cdb68abb2790f0099d570e5c07f
Levani1111 <papashvililevani@gmail.com>
2022-03-18 13:38:36 -0400

    style header with logo and menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Headre() {
+  const menuItems = [
+    {
+      title: "Home",
+      key: "/",
+    },
+    {
+      title: "Projects",
+      key: "/projects",
+    },
+    {
+      title: "Course",
+      key: "/course",
+    },
+    {
+      title: "Contact",
+      key: "/contact",
+    },
+  ];
+
   return (
-    <div>
-      <h1 className='bg-theme  font-mont text-white'>Header component</h1>
+    <div className="text-white font-mont">
+      <div className="flex bg-theme justify-between items-center p-2 shadow-lg">
+        <div>
+          <h1 className="text-4xl font-semibold">L P</h1>
+        </div>
+        <div className="flex">
+          {menuItems.map((item) => {
+            return (
+              <li className="list-none mx-5">
+                <Link to={`/${item.key}`}>{item.title}</Link>
+              </li>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
